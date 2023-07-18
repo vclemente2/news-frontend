281205ac85ed31c15a3aa21603bc5d8dd799a9a7
Vinicius Bastos <vclemente2@hotmail.com>
2023-07-18 20:52:54 -0300

    Criação do modelo de noticias.

---
+++ src/js/models/NewsModel.js
@@ -0,0 +1,52 @@
+import { http } from "../config/http";
+
+export class NewsModel {
+  #title;
+  #description;
+  #image;
+  #category;
+
+  constructor(title, description, category, image) {
+    this.#title = title;
+    this.#description = description;
+    this.#category = category;
+    this.#image = image;
+  }
+
+  static create(newsTitle, newsDescription, category, newsImage = null) {
+    return new NewsModel(newsTitle, newsDescription, category, newsImage);
+  }
+
+  get data() {
+    return {
+      title: this.#title,
+      description: this.#description,
+      image: this.#image,
+      category: this.#category
+    };
+  }
+
+  async findAll() {
+    try {
+      return await http.get("/news");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async findOne(id) {
+    try {
+      return await http.get("/news", { params: id });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async save() {
+    try {
+      return await http.post("/news", { ...this.data });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
