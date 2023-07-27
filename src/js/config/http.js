85398e6ae76d64ce325ee128cd092c72c97f67aa
Vinicius Bastos <vclemente2@hotmail.com>
2023-07-27 12:41:54 -0300

    Ajuste no grid dos cards de notícias.

--- src/js/config/http.js
+++ src/js/config/http.js
@@ -1,4 +1,4 @@
 export const http = axios.create({
-  // baseURL: "https://cute-tan-buffalo-cuff.cyclic.app/"
-  baseURL: "http://localhost:3000"
+  baseURL: "https://cute-tan-buffalo-cuff.cyclic.app/"
+  // baseURL: "http://localhost:3000"
 });
